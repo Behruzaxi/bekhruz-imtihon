a39a7f5bf6a6d1694d84999323c7bac90e0c5e33
Behruzaxi <ebehruz846@gmail.com>
2023-04-09 21:29:19 +0500

    fdsa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
 import Cred from './components/Cred/Cred'
-import Allbabyis from './components/AllBaby/Allbabyis'
-import Futal from './components/Footer/Futal'
 import Navboal from './components/Navbar/Navboal'
 import Cardsbex from './components/Shopcards/Cardsbex'
+import Subex from './components/Subex'
 
 export default function App() {
   return (
@@ -13,11 +12,10 @@ export default function App() {
       <Navboal/>
         <Routes>
           <Route path="/Create" element={<Cred/>} />
-          <Route path="/home" element={<Allbabyis/>} />
+          <Route path="/home" element={<Subex/>} />
           <Route path="/Shop" element={<Cardsbex/>} />
         </Routes>
       </Router>
-      <Futal/>
     </>
   )
 }
---
+++ src/components/Subex.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Allbabyis from './AllBaby/Allbabyis'
+import Futal from './Footer/Futal'
+
+export default function Subex() {
+  return (
+    <>
+    <Allbabyis/>
+    <Futal/>
+    </>
+    )
+}
